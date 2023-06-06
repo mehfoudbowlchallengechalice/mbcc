11a30f81a2f5f219d27b110eb133d79de9778f2c
mehfoudbowlchallengechalice <135705247+mehfoudbowlchallengechalice@users.noreply.github.com>
2023-06-06 15:35:44 -0400

    Update app.py

--- app.py
+++ app.py
@@ -35,9 +35,7 @@ def run_query(query):
 sheet_url = st.secrets["private_gsheets_url"]
 rows = run_query(f'SELECT * FROM "{sheet_url}"')
 
-for row in rows:
-    st.write(row)
-    
+
     
 st.dataframe(rows)
 
