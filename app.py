f58453955ecc2832ab495ab56730d83af4db75b2
mehfoudbowlchallengechalice <135705247+mehfoudbowlchallengechalice@users.noreply.github.com>
2023-06-06 15:22:46 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pandas as pd
 import pandasql as ps
 from google.oauth2 import service_account
-import gspread
 import numpy as np
 import matplotlib.pyplot as plt
 import plotly.figure_factory as ff
@@ -10,7 +9,29 @@ import plotly.express as px
 import plotly.graph_objs as go
 from plotly.subplots import make_subplots
 from sklearn.linear_model import LogisticRegression
+import streamlit as st
+from google.oauth2 import service_account
+from gsheetsdb import connect
 
 st.write ("""
 ## THE BASE COAT FOR MBCC 12 in 23-24!!
 """)
+
+# Create a connection object.
+credentials = service_account.Credentials.from_service_account_info(
+    st.secrets["gcp_service_account"],
+    scopes=[
+        "https://www.googleapis.com/auth/spreadsheets",
+    ],
+)
+conn = connect(credentials=credentials)
+
+@st.cache_data(ttl=600)
+def run_query(query):
+    rows = conn.execute(query, headers=1)
+    rows = rows.fetchall()
+    return rows
+  
+sheet_url = st.secrets["private_gsheets_url"]
+rows = run_query(f'SELECT * FROM "{sheet_url}"')
+
