b006b6b5461762c27e1dddbc67616611f88be950
mehfoudbowlchallengechalice <135705247+mehfoudbowlchallengechalice@users.noreply.github.com>
2023-08-30 16:52:36 -0400

    Update app.py

--- app.py
+++ app.py
@@ -94,9 +94,9 @@ with tabtoday:
 		option_ln = st.checkbox("Lauren", value = True)
 	with checks[4]:
 		option_ns = st.checkbox("Nicholas", value = True)
-		option_pf = st.checkbox("P Smurf", value = True)
+		option_pf = st.checkbox("PSmurf", value = True)
 
-	people_list = ["Christopher", "Elise", "Emma", "Gregory", "Jen", "Joseph", "Laura", "Lauren", "Nicholas", "P Smurf"]
+	people_list = ["Christopher", "Elise", "Emma", "Gregory", "Jen", "Joseph", "Laura", "Lauren", "Nicholas", "PSmurf"]
 	collection_df = pd.DataFrame(list(zip(people_list, [option_cr, option_ee, option_ea, option_gy, option_jn, option_jh, option_la, option_ln, option_ns, option_pf])))
 	selection_list = collection_df[collection_df[1] == True][0].values.tolist()
 	selection_list = np.insert(selection_list, 0, 'Game')
