212f08d4f2913ec0defb228a512184aa5a8c3257
mehfoudbowlchallengechalice <135705247+mehfoudbowlchallengechalice@users.noreply.github.com>
2023-10-21 17:03:52 -0400

    Update toggles.py

--- toggles.py
+++ toggles.py
@@ -2,29 +2,27 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 
-## i creates the version of toggles so that we can use this function for multiple toggles
+
 def toggle_list(i):
 	checks = st.columns(5)
 	with checks[0]:
-		exec("option_cr"+i+" = st.toggle('Christopher', value = True)", globals())
-		exec("option_ee"+i+" = st.toggle('Elise', value = True)", globals())
+		option_cr = st.toggle("Christopher", value = True, key = "cr"+i)
+		option_ee = st.toggle("Elise", value = True, key = "ee"+i)
 	with checks[1]:
-		exec("option_ea"+i+" = st.toggle('Emma', value = True)", globals())
-		exec("option_gy"+i+" = st.toggle('Gregory', value = True)", globals())
+		option_ea = st.toggle("Emma", value = True, key = "ea"+i)
+		option_gy = st.toggle("Gregory", value = True, key = "gy"+i)
 	with checks[2]:
-		exec("option_jn"+i+" = st.toggle('Jen', value = True)", globals())
-		exec("option_jh"+i+" = st.toggle('Joseph', value = True)", globals())
+		option_jn = st.toggle("Jen", value = True, key = "jn"+i)
+		option_jh = st.toggle("Joseph", value = True, key = "jh"+i)
 	with checks[3]:
-		exec("option_la"+i+" = st.toggle('Laura', value = True)", globals())
-		exec("option_ln"+i+" = st.toggle('Lauren', value = True)", globals())
+		option_la = st.toggle("Laura", value = True, key = "la"+i)
+		option_ln = st.toggle("Lauren", value = True, key = "ln"+i)
 	with checks[4]:
-		exec("option_ns"+i+" = st.toggle('Nicholas', value = True)", globals())
-		exec("option_pf"+i+" = st.toggle('PSmurf', value = True)", globals())
+		option_ns = st.toggle("Nicholas", value = True, key = "ns"+i)
+		option_pf = st.toggle("PSmurf", value = True, key = "pf"+i)
 
-	exec("variable_list = [option_cr"+i+", option_ee"+i+", option_ea"+i+", option_gy"+i+", option_jn"+i+", option_jh"+i+", option_la"+i+", option_ln"+i+", option_ns"+i+", option_pf"+i+"]", globals())
-	print(variable_list)
 	people_list = ["Christopher", "Elise", "Emma", "Gregory", "Jen", "Joseph", "Laura", "Lauren", "Nicholas", "PSmurf"]
-	collection_df = pd.DataFrame(list(zip(people_list, variable_list)))
+	collection_df = pd.DataFrame(list(zip(people_list, [option_cr, option_ee, option_ea, option_gy, option_jn, option_jh, option_la, option_ln, option_ns, option_pf])))
 	selection_list = collection_df[collection_df[1] == True][0].values.tolist()
 	return selection_list
         
