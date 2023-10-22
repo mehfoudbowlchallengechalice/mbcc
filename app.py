87b8bed9a16a796b34840a721efab7841ba02ce3
mehfoudbowlchallengechalice <135705247+mehfoudbowlchallengechalice@users.noreply.github.com>
2023-10-22 16:50:12 -0400

    Update app.py

--- app.py
+++ app.py
@@ -165,7 +165,7 @@ with tab1:
 	win_per_player = []
 	
 	for player in starting_people_list:
-		#win_per_player_specific = []
+		win_per_player_specific = []
 		win_per_player_specific.append(remaining_df[player])
 		st.dataframe(win_per_player_specific)
 	st.dataframe(remaining_df)
