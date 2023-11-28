1611c64cbd304702549b69d19792d3409a460982
mehfoudbowlchallengechalice <135705247+mehfoudbowlchallengechalice@users.noreply.github.com>
2023-11-27 19:49:44 -0500

    Update app.py

--- app.py
+++ app.py
@@ -298,16 +298,15 @@ with tab_history:
 # 	st.dataframe(ps.sqldf(text_input))
 
 with need_to_know:
-	st.header("Information")
-	st.markdown("""
-	###Objective:
- 	-	Pick the most games correctly. If you pick more correctly than anyone else, you win.
+	#st.header("Information")
+	"""
+ 	###Objective:
+	-	Pick the most games correctly. If you pick more correctly than anyone else, you win.
   	-	Root for your teams to win by as much as possible or lose by as few as possible when winning isn't an option.
    		-	In betting, your team can cover without winning, that's what this represents
-	"""
-	)
 	
-	st.markdown("""
+	
+	
 	###Tiebreakers (only decides 1st vs 2nd):
 	1.	Point Spread Points
 	1.	Upset Pick Points
@@ -317,7 +316,7 @@ with need_to_know:
 	1.	Point Differential in Playoff Picks
 	1.	Point Differential in Potential Upset Picks
  	1.	MBCC Commemorative Coin Flip
-	""")
+	"""
 	
 		
 	##TODO drop down for specific MBCC
