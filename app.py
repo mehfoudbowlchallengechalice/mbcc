b4e12d6c302516460585906791490ddbe782e968
mehfoudbowlchallengechalice <135705247+mehfoudbowlchallengechalice@users.noreply.github.com>
2023-12-15 17:06:22 -0500

    Update app.py

--- app.py
+++ app.py
@@ -150,8 +150,9 @@ with tab_today:
 		new_live_df = new_live_df[(pd.to_datetime(new_live_df.game_date) >= datetime.datetime.today()) 
         			& (pd.to_datetime(new_live_df.game_date) == min(pd.to_datetime(new_live_df.game_date)))] 
 		st.dataframe(new_live_df.style.apply(highlight_all_games, axis=None), column_config={"game_page": st.column_config.LinkColumn()}, hide_index=True)
-	
-    st.write("* upset picks are :blue[blue] and unanimous picks are :grey[grey].")
+        
+        
+	st.write("* upset picks are :blue[blue] and unanimous picks are :grey[grey].")
 	st.markdown("""---""")
 	st.header("Picks")
 	
