7dd0145b53c69f22e569ba93b50bfc9968953107
mehfoudbowlchallengechalice <135705247+mehfoudbowlchallengechalice@users.noreply.github.com>
2023-12-16 09:01:25 -0500

    Update app.py

--- app.py
+++ app.py
@@ -152,7 +152,7 @@ with tab_today:
 		st.dataframe(new_live_df.style.apply(highlight_all_games, axis=None), column_config={"game_page": st.column_config.LinkColumn(), "team_focus_indicator": None}, hide_index=True)
 	elif option == "Today":
 		new_live_df = new_live_df[(pd.to_datetime(new_live_df.game_date) >= actual_today) 
-        			& (pd.to_datetime(new_live_df.game_date) == min(pd.to_datetime(new_live_df.game_date)))] 
+        			| (pd.to_datetime(new_live_df.game_date) == min(pd.to_datetime(new_live_df.game_date)))] 
 		st.dataframe(new_live_df.style.apply(highlight_all_games, axis=None), column_config={"game_page": st.column_config.LinkColumn(), "team_focus_indicator": None}, hide_index=True)
         
         
