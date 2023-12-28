a0d828a2209dc850ea9b22e5c9743b90ba9a1cc8
mehfoudbowlchallengechalice <135705247+mehfoudbowlchallengechalice@users.noreply.github.com>
2023-12-27 21:57:39 -0500

    Update app.py

--- app.py
+++ app.py
@@ -237,7 +237,10 @@ with tab_elimination:
 		comparison_dict = {}
 		for compare_player in the_people_list:
 			player_picks = set(remaining_df[remaining_df['gametracker']==0]["Game"]+remaining_df[remaining_df['gametracker']==0][compare_player])
+			st.write(player_picks)
 			# checks to see if the player's potential picks line up comparing players
+			comparison_array = player_picks & potential_correct_picks
+			st.write(comparison_array)
 			comparison_dict[compare_player] = len(player_picks & potential_correct_picks)+player_current_picks.astype(int)
 
 		full_elimination_matrix[player] = comparison_dict
