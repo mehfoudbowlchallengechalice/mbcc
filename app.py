e6223e3a31f3273fb5780799f05d89428177059e
mehfoudbowlchallengechalice <135705247+mehfoudbowlchallengechalice@users.noreply.github.com>
2024-11-25 13:36:28 -0500

    Update app.py

--- app.py
+++ app.py
@@ -22,11 +22,11 @@ from PIL import Image
 st.set_page_config(layout="wide")
 
 st.header ("""
-MBCC 12 in 2023-24
+MBCC 13 in 2024-25
 """)
 
-#pick_url = 'https://forms.gle/QBjfisMMErnJsajg9'
-#st.header ("Make your [picks](%s) before December 15th at 12 PM!" % pick_url)
+pick_url = 'https://forms.gle/QBjfisMMErnJsajg9'
+st.header ("Make your [picks](%s) before December 15th at 12 PM!" % pick_url)
 #st.header("The Picks are IN! - HERE WE GO!")
 #st.header("Day 1 - 7 games - Upset Pick City!")
 #st.header("Day 2 - What a comeback win to cut the first place cluster down to two!")
@@ -41,7 +41,8 @@ MBCC 12 in 2023-24
 #st.header("Day 11 - The points race is heating up, you want your pick to win BIG")
 #st.header("Day 12 - The Hokie Prize is still in reach for everyone!")
 #st.header("Day 13 - Christopher takes the Crown before the Championship, but there's still a reason to watch!")
-st.header("MBCC 12 Champion - Christopher; MBCC 12 Second Place - Joseph")
+#st.header("MBCC 12 Champion - Christopher; MBCC 12 Second Place - Joseph")
+st.header("LET THE PLAYOFFS BEGIN!")
 
 ##TODO drop down here for players (create player tab?)
 ## set option to have current winner in bold
