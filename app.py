a6ff577e9a901d3c9332903c9553e40668d460a8
mehfoudbowlchallengechalice <135705247+mehfoudbowlchallengechalice@users.noreply.github.com>
2024-11-27 16:53:53 -0500

    Update app.py

--- app.py
+++ app.py
@@ -141,7 +141,8 @@ with tab_today:
 	#live_df = bring_in_live_games()
 	#live_df = pd.DataFrame(run_query(f'SELECT * FROM "{unlive_games}"'))
 	live_df = unlive_games
-
+	unlive_games['game_date'] = pd.to_datetime(unlive_games['game_date'])
+	unlive_games['game_time'] = pd.to_datetime(unlive_games['game_time'])
 	
 	live_df['game_page'] = "https://www.espn.com/college-football/game?gameId="#+live_df['game_id'].astype(int).astype(str)
 	st.write(live_df['game_time'])
