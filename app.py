0fc15ed428ab9526c053937e0b056ebf9c04b31f
mehfoudbowlchallengechalice <135705247+mehfoudbowlchallengechalice@users.noreply.github.com>
2024-12-17 13:16:22 -0500

    Update app.py

--- app.py
+++ app.py
@@ -177,7 +177,7 @@ with tab_today:
 	actual_today = pd.to_datetime((datetime.datetime.today()-pd.Timedelta(hours=5)).strftime('%Y-%m-%d'))
 	#st.write(actual_today)
 	### today, future, all drop down to show picks
-	option = st.selectbox("Select Games to See", ("All", "Today", "Future"))
+	option = st.selectbox("Select Games to See", ("Today", "Future", "All"))
 	
 	if option == "All":
 		st.dataframe(new_live_df.style.apply(highlight_all_games, axis=None), column_config={"game_page": st.column_config.LinkColumn(), "team_focus_indicator": None}, hide_index=True)
