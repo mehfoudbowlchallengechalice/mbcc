42c4b877b9f088276a352bdb82db38107941a87d
mehfoudbowlchallengechalice <135705247+mehfoudbowlchallengechalice@users.noreply.github.com>
2025-12-07 08:17:02 -0500

    Update app.py

--- app.py
+++ app.py
@@ -225,10 +225,8 @@ with tab_today:
 			except:
 				new_live_df
 			st.dataframe(new_live_df.style.apply(highlight_all_games, axis=None), column_config={"game_page": st.column_config.LinkColumn(), "team_focus_indicator": None}, hide_index=True)
-            
         st.markdown("* upset pick games are :blue[blue] with the underdog seen in :blue[blue] and unanimously picked games are :grey[grey] with the favorite in :green[green].")
-	    
-		st.markdown("""---""")
+	    st.markdown("""---""")
 		st.header("Picks")
 		
 		selection_list_p, deselection_list_p = toggle_list("a")
